24c7e75f37e8ae639da4608186e96b6153e89937
Spyros Maniatopoulos <spmaniato@gmail.com>
2017-01-13 22:56:31 -0600

    [rosserial_python] Don't print unconfigured topic

--- rosserial_python/src/rosserial_python/SerialClient.py
+++ rosserial_python/src/rosserial_python/SerialClient.py
@@ -506,7 +506,7 @@ class SerialClient:
                     try:
                         self.callbacks[topic_id](msg)
                     except KeyError:
-                        rospy.logerr("Tried to publish before configured. Topic id: %d, name: %s", topic_id, topic_name)
+                        rospy.logerr("Tried to publish before configured. Topic id: %d", topic_id)
                     rospy.sleep(0.001)
                 else:
                     rospy.loginfo("Wrong checksum for topic id and msg. Topic id: %d, name: %s", topic_id, topic_name)
